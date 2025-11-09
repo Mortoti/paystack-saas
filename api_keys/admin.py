26727942211a037c6471fe60bb914bc6ba8b7313
Mortoti <mortoti.dev@gmail.com>
2025-11-09 14:53:18 +0000

    APIKey Model Registration in the Admin

--- api_keys/admin.py
+++ api_keys/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import APIKey
 
-# Register your models here.
+
+@admin.register(APIKey)
+class APIKeyAdmin(admin.ModelAdmin):
+    list_display = ['name', 'user', 'key_preview', 'is_active', 'created_at', 'last_used']
+    list_filter = ['is_active', 'created_at']
+    search_fields = ['name', 'user__username', 'key']
+    readonly_fields = ['key', 'created_at', 'last_used']
+
+    def key_preview(self, obj):
+        return f"{obj.key[:20]}..."
+
+    key_preview.short_description = 'API Key'
